9452c05af9af277af6c2360a5f93d417fe9563c2
Peter <pshtif@gmail.com>
2022-10-27 23:08:50 +0200

    add baking enum

---
+++ Runtime/Scripts/Enums/VoxelBakeTransform.cs
@@ -0,0 +1,13 @@
+﻿/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+namespace BinaryEgo.Voxelizer
+{
+    public enum VoxelBakeTransform
+    {
+        ALL,
+        SCALE_AND_ROTATION,
+        NONE
+    }
+}
