9596801ede7df559d9906209e6b6f39c4ab58152
Peter <pshtif@gmail.com>
2022-10-31 16:03:36 +0100

    separate job structs

---
+++ Runtime/Scripts/Jobs/VoxelPositionUpdateJob.cs
@@ -0,0 +1,29 @@
+﻿/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace BinaryEgo.Voxelizer
+{
+    [BurstCompile]
+    public struct VoxelPositionUpdateJob : IJobParallelFor
+    {
+        [ReadOnly] public Matrix4x4 previousTransformMatrixI;
+        [ReadOnly] public Matrix4x4 transformMatrix;
+        //[ReadOnly] public NativeList<Vector4> c;
+
+        public NativeArray<Matrix4x4> matrices;
+        //public NativeSlice<Vector4> colors;
+
+        public void Execute(int index)
+        {
+            Matrix4x4 matrix = previousTransformMatrixI * matrices[index];
+            matrices[index] = transformMatrix * matrix;
+            //colors[index] = c[index];
+        }
+    }
+}
