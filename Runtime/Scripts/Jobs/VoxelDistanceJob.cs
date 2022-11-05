4798e1371acfd99b96fde121bf4ba889d6aa3dc1
Peter <pshtif@gmail.com>
2022-11-05 11:08:20 +0100

    raycasting / distance jobs

---
+++ Runtime/Scripts/Jobs/VoxelDistanceJob.cs
@@ -0,0 +1,24 @@
+﻿/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace BinaryEgo.Voxelizer
+{
+    [BurstCompile(CompileSynchronously = true)]
+    public struct VoxelDistanceJob : IJobParallelForFilter
+    {
+        public Vector3 point;
+        public float radius;
+        [ReadOnly] public NativeArray<Matrix4x4> inMatrices;
+
+        public bool Execute(int p_index)
+        {
+            return (inMatrices[p_index].GetPosition() - point).magnitude < radius;
+        }
+    }
+}
