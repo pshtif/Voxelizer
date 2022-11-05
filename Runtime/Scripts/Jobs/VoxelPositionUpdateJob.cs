e14fafe7a457f914460b4f102eca02b7b50c68f8
Peter <pshtif@gmail.com>
2022-11-05 11:10:14 +0100

    job update

--- Runtime/Scripts/Jobs/VoxelPositionUpdateJob.cs
+++ Runtime/Scripts/Jobs/VoxelPositionUpdateJob.cs
@@ -9,20 +9,20 @@ using UnityEngine;
 
 namespace BinaryEgo.Voxelizer
 {
-    [BurstCompile]
+    [BurstCompile(CompileSynchronously = true)]
     public struct VoxelPositionUpdateJob : IJobParallelFor
     {
         [ReadOnly] public Matrix4x4 previousTransformMatrixI;
         [ReadOnly] public Matrix4x4 transformMatrix;
         //[ReadOnly] public NativeList<Vector4> c;
 
-        public NativeArray<Matrix4x4> matrices;
+        public NativeArray<Matrix4x4> inOutMatrices;
         //public NativeSlice<Vector4> colors;
 
-        public void Execute(int index)
+        public void Execute(int p_index)
         {
-            Matrix4x4 matrix = previousTransformMatrixI * matrices[index];
-            matrices[index] = transformMatrix * matrix;
+            Matrix4x4 matrix = previousTransformMatrixI * inOutMatrices[p_index];
+            inOutMatrices[p_index] = transformMatrix * matrix;
             //colors[index] = c[index];
         }
     }
